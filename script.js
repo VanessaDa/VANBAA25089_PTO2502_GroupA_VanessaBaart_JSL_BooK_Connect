814e44132a17c4a3803350a20536324c860afd1a
Vanessa Gwama <vanessagwama@MacBookAir.home>
2025-06-05 12:10:46 +0200

    Add comparison and logical examples

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 //Comparison operators
 
-5 == 5 // true
-5 === '5' // false
-5 > 6 // false
-5 < 6 // true
-5 >= 5 // true
-5 <= 6 // true
-
-console.log(5 == 5);
-console.log(5 === "5");
-console.log(5 > 6);
-console.log(5 < 6);
-console.log(5 >= 5);
-console.log(5 <= 6);
+5 == 5; // true
+5 === "5"; // false
+5 > 6; // false
+5 < 6; // true
+5 >= 5; // true
+5 <= 6; // true
 
 //Logical operators
-
 const a = true;
-const b = true;
+const b = false;
 
-console.log(a && b);
+console.log(!a || b);
+
+
