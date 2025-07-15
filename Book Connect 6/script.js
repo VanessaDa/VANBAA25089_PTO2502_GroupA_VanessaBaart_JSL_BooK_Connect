0d6189071b820c4e69d941c0a6ea262154a00597
Vanessa Gwama <vanessagwama@MacBookAir.home>
2025-07-15 13:19:00 +0200

    Add Book connect folder

---
+++ Book Connect 6/script.js
@@ -0,0 +1,16 @@
+//Comparison operators
+
+5 == 5; // true
+5 === "5"; // false
+5 > 6; // false
+5 < 6; // true
+5 >= 5; // true
+5 <= 6; // true
+
+//Logical operators
+const a = true;
+const b = false;
+
+console.log(!a || b);
+
+
