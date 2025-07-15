3d251fda5b454222b6aaf97e38cbd5df2011fa16
Vanessa Gwama <vanessagwama@MacBookAir.home>
2025-07-15 13:15:52 +0200

    Add expressions, statements, conditionals examples

---
+++ Book Connect 7/script.js
@@ -0,0 +1,26 @@
+//Expressions and Statements
+let count = 5; //let count = is a statement which is used to define
+               //a variable named 'count' while '5' is an expression
+
+//Examples of expressions
+5 * 5;
+5 < 15;
+false;
+
+{
+//Conditional and Logical operators
+let x = 10;
+if (x > 6) {
+    //Do something
+}
+//Otherwise continue
+
+//Conditional Operators
+console.log(x == "10");   // true
+console.log(x === "10");  // false
+console.log(x != "10");   // true
+console.log(x > 6);       // true
+console.log(x < 6);       // false
+console.log(x >= 10);     // true
+console.log(x <= 10);     // true
+}
